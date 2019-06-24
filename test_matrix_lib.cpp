c8ea016f633a6224dace9f336c47d3755b955b7e
Svetlana <bessveta@gmail.com>
2019-06-25 02:11:44 +0300

    add test

---
+++ test_matrix_lib.cpp
@@ -0,0 +1,38 @@
+/*
+ * test_matrix_lib.cpp
+ *
+ *  Created on: 25 июн. 2019 г.
+ *      Author: sveta
+ */
+
+/*
+ * test_ip_filter.cpp
+ *
+ *  Created on: 10 мая 2019 г.
+ *      Author: sveta
+ */
+#define BOOST_TEST_MODULE ip_filter_test_module
+#include <boost/test/included/unit_test.hpp>
+#include "matrix_lib.h"
+
+BOOST_AUTO_TEST_SUITE(matrix_test_suite)
+
+BOOST_AUTO_TEST_CASE(matrix_test_case)
+{
+	Matrix<int, 0> mtrx;
+	auto n = 10;
+
+	for (auto i = 0; i < n; i++) {
+		mtrx[i][i] = i;
+	}
+	BOOST_REQUIRE_EQUAL(mtrx.size(),n-1);
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
+
